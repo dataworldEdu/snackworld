b1f9d3fb2a052016bb0aaf2649524c72b1543e0e
KimJungWon1 <rlawjddnjs753@dataworld.co.kr>
2022-04-07 12:57:52 +0900

    modify loginForm.jsp

--- src/main/java/edu/dataworld/snackworld/main/web/MainController.java
+++ src/main/java/edu/dataworld/snackworld/main/web/MainController.java
@@ -20,4 +20,9 @@ public class MainController {
     public String showMain() {
         return "/home/main.view";
     }
+
+    @RequestMapping(value="/login/loginForm")
+    public String showLogin() {
+        return "/login/loginForm.view";
+    }
 }
