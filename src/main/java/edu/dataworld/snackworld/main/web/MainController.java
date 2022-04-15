221cc26bc939284ede19c9374bdd1b3edf19c032
duddk1551 <duddk1551@gmail.com>
2022-04-15 15:55:15 +0900

    modify

--- src/main/java/edu/dataworld/snackworld/main/web/MainController.java
+++ src/main/java/edu/dataworld/snackworld/main/web/MainController.java
@@ -18,10 +18,10 @@ public class MainController {
         return "redirect:/login/loginForm.do";
     }
 
-//    @RequestMapping(value="/home/main.do")
-//    public String showMain() {
-//        return "/home/main.view";
-//    }
+    @RequestMapping(value="/home/main.do")
+    public String showMain() {
+        return "/home/main.view";
+    }
 
 //    @RequestMapping(value="/home/main.do", produces="text/plain;charset=UTF-8")
 //    @ResponseBody   //없으면 AJAX 통신 안되요
